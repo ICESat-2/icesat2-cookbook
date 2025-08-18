364e2651673a3bd4f9758336ab445caa9998eccf
tsutterley <5384907+tsutterley@users.noreply.github.com>
2025-08-18 23:19:52 +0000

    Delete preview for pull request \#15

--- _preview/15/build/_shared/latex-UV6YOLNM.js
+++
@@ -1,1 +0,0 @@
-import{a}from"/icesat2-cookbook/_preview/15/build/_shared/chunk-SFYLH6CB.js";import"/icesat2-cookbook/_preview/15/build/_shared/chunk-2NH4LW52.js";export default a();
--- _preview/15/build/_shared/sas-XXTVPG3N.js
+++
@@ -1,1 +0,0 @@
-import{a}from"/icesat2-cookbook/_preview/15/build/_shared/chunk-CSXXHIMG.js";import"/icesat2-cookbook/_preview/15/build/_shared/chunk-2NH4LW52.js";export default a();
