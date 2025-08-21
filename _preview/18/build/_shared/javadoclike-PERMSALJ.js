3a8e669d8550bb74bcfb272bba80c8e7ca3ace58
aaarendt <4993098+aaarendt@users.noreply.github.com>
2025-08-21 22:52:57 +0000

    Delete preview for pull request \#18

--- _preview/18/build/_shared/javadoclike-PERMSALJ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"/icesat2-cookbook/_preview/18/build/_shared/chunk-ZOXRFJWB.js";import"/icesat2-cookbook/_preview/18/build/_shared/chunk-2NH4LW52.js";export default a();
--- _preview/18/build/_shared/julia-LADIOEYJ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"/icesat2-cookbook/_preview/18/build/_shared/chunk-UAR2IYX3.js";import"/icesat2-cookbook/_preview/18/build/_shared/chunk-2NH4LW52.js";export default a();
