2a6677a1154fc91f3801fd3bcee04179c5c99f24
scottyhq <scottyhq@users.noreply.github.com>
2025-08-22 17:59:54 +0000

    Deploying to gh-pages from @ ICESAT-2HackWeek/icesat2-cookbook@11c7dafe9beeac74763340fda3848b2a9cc43a94 🚀

---
+++ _preview/22/build/_shared/xquery-PKKNDEQP.js
@@ -0,0 +1,1 @@
+import{a}from"/icesat2-cookbook/_preview/22/build/_shared/chunk-QQKFE3ZI.js";import"/icesat2-cookbook/_preview/22/build/_shared/chunk-2NH4LW52.js";export default a();
---
+++ _preview/22/build/cryocloud-a116c80df2d4a01bad22c52d5705257b.md
@@ -0,0 +1,6 @@
+# CryoCloud
+
+The following page is rendered from https://book.cryointhecloud.com
+
+:::{embed} xref:cryocloud
+:::
